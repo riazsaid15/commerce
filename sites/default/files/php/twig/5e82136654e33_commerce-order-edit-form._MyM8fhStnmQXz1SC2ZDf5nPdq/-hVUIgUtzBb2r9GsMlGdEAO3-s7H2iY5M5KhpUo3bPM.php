4c137d12852db489f9ca9fccf4b9a33fe29109fd
unknown <riazsaid15@yahoo.com>
2020-03-30 21:27:36 +0500

    comment approval issue solved

---
+++ sites/default/files/php/twig/5e82136654e33_commerce-order-edit-form._MyM8fhStnmQXz1SC2ZDf5nPdq/-hVUIgUtzBb2r9GsMlGdEAO3-s7H2iY5M5KhpUo3bPM.php
@@ -0,0 +1,105 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/commerce/modules/order/templates/commerce-order-edit-form.html.twig */
+class __TwigTemplate_c01d9e757c19bd017a6769dee044e144db3dc2bf48fec4792b490dfeb8c15128 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = ["escape" => 14, "without" => 14];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape', 'without'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 12
+        echo "<div class=\"layout-order-form clearfix\">
+  <div class=\"layout-region layout-region-order-main\">
+    ";
+        // line 14
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->env->getExtension('Drupal\Core\Template\TwigExtension')->withoutFilter($this->sandbox->ensureToStringAllowed(($context["form"] ?? null)), "advanced", "actions"), "html", null, true);
+        echo "
+  </div>
+  <div class=\"layout-region layout-region-order-secondary\">
+    ";
+        // line 17
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["form"] ?? null), "advanced", [])), "html", null, true);
+        echo "
+  </div>
+  <div class=\"layout-region layout-region-order-footer\">
+    ";
+        // line 20
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["form"] ?? null), "actions", [])), "html", null, true);
+        echo "
+  </div>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/commerce/modules/order/templates/commerce-order-edit-form.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  71 => 20,  65 => 17,  59 => 14,  55 => 12,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/contrib/commerce/modules/order/templates/commerce-order-edit-form.html.twig", "C:\\xampp\\htdocs\\commerce\\modules\\contrib\\commerce\\modules\\order\\templates\\commerce-order-edit-form.html.twig");
+    }
+}
